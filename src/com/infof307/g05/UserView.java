747a3ddecdd54be3448030c221bc7fd3f7d368dc
YAMANI Imad Eddine <imad.yamani@ulb.be>
2019-02-28 15:11:29 +0100

    Added some Javadoc for User view

--- src/com/infof307/g05/UserView.java
+++ src/com/infof307/g05/UserView.java
@@ -1,6 +1,16 @@
 package com.infof307.g05;
 
+/**
+ * User view
+ * @author @iyamani
+ * @codereview @Tanvirul.Hoque
+ */
 public class UserView {
+    /**
+     * prints the user details (username and email)
+     * @param username
+     * @param email
+     */
     public void printUserDetails(String username, String email) {
         System.out.println("Username: " + username);
         System.out.println("Email: " + email);
