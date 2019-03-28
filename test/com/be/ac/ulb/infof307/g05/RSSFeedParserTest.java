4cac7de166644a6b53884f13b2f6ff272f24f095
Orestis Tranganidas <otrangan@ulb.ac.be>
2019-03-28 13:39:26 +0000

    Upload RSSFeedParserTest Coopilotes Tombou Vincent, Tanvirul Hoque

---
+++ test/com/be/ac/ulb/infof307/g05/RSSFeedParserTest.java
@@ -0,0 +1,25 @@
+package com.be.ac.ulb.g05;
+
+import com.be.ac.ulb.g05.Model.Article;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class RSSFeedParserTest extends TestCase {
+
+    /**
+     * Tests if the RSSFeedParser returns a list of articles
+     */
+    @Test
+    public void testReadRSS(){
+        String[] websites = {"http://sport24.lefigaro.fr/rssfeeds/sport24-accueil.xml", "https://www.lemonde.fr/m-actu/rss_full.xml",
+                "https://www.lepoint.fr/24h-infos/rss.xml", "https://www.theguardian.com/uk/business/rss",
+                "https://feeds.feedburner.com/rtlinfo/belgique"};
+        for(int i=0;i<5;++i){
+            RSSFeedParser parser = new RSSFeedParser(websites[i]);
+            ArrayList<Article> articles = parser.readRSS();
+            assertTrue(articles.size()>0);
+        }
+    }
+}
