fa98369adf12c2cc39f14e87c47e6d6d926d3ad5
Orestis Tranganidas <otrangan@ulb.ac.be>
2019-03-28 13:37:57 +0000

    Upload ParserWebSiteTest Coopilotes Tombou Vincent, Tanvirul Hoque

---
+++ test/com/be/ac/ulb/infof307/g05/ParserWebSiteTest.java
@@ -0,0 +1,45 @@
+package com.be.ac.ulb.g05;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ParserWebSiteTest extends TestCase {
+
+    /**
+     * Tests if the ParserArticle parses the article
+     */
+    @Test
+    public void testParserArticle(){
+        ParserWebSite web = new ParserWebSite();
+        try {
+            String article = web.ParserArticle("https://www.theguardian.com/world/2019/mar/28/bangladesh-high-rise-fire-dhaka-prompts-people-to-jump-from-building");
+            assertFalse(article == "");
+
+            article = web.ParserArticle("https://www.theguardian.com/world/2019/mar/28/bangladesh-high-rise-fire-dhaka-prompts-people-to-jump-from-buildin");
+            assertTrue(article == "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Tests if the ParserImage parses the image
+     */
+    @Test
+    public void testParserImage(){
+        ParserWebSite web = new ParserWebSite();
+        try {
+            String image = web.ParserImage("https://www.theguardian.com/cities/2019/mar/28/kumbh-mela-cleaning-up-after-the-worlds-largest-human-gathering");
+            assertFalse(image == "");
+
+            image = web.ParserImage("https://www.theguardian.com/cities/2019/mar/28/kumbh-mela-cleaning-up-after-the-worlds-largest-human-gatherin");
+            assertTrue(image == "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
