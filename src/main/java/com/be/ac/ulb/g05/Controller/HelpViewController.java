a58617b2370a5be7566c3a3d97fa4d95745bf6af
YAMANI Imad Eddine <imad.yamani@ulb.be>
2019-05-03 14:39:10 +0200

    Updated HelpView

--- src/main/java/com/be/ac/ulb/g05/Controller/HelpViewController.java
+++ src/main/java/com/be/ac/ulb/g05/Controller/HelpViewController.java
@@ -1,5 +1,10 @@
 package com.be.ac.ulb.g05.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.media.MediaView;
+
 public class HelpViewController {
 
+    @FXML
+    public MediaView helpVideo;
 }
