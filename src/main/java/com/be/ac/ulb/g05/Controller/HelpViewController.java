649bc67baa2454a775293b5022849240216e3de3
YAMANI Imad Eddine <imad.yamani@ulb.be>
2019-05-12 15:01:12 +0200

    Added Help video

--- src/main/java/com/be/ac/ulb/g05/Controller/HelpViewController.java
+++ src/main/java/com/be/ac/ulb/g05/Controller/HelpViewController.java
@@ -1,10 +1,23 @@
 package com.be.ac.ulb.g05.Controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.media.MediaView;
 
-public class HelpViewController {
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
+public class HelpViewController extends AbstractController {
 
     @FXML
-    public MediaView helpVideo;
+    public WebView webView;
+
+    private String embedVideo = "https://youtube.com/embed/PImqC9Lkt2w";
+
+    /**
+     * Sets up the view. Called the first time UI element is loaded
+     */
+    @Override
+    public void setupView() {
+        webView.getEngine().load(embedVideo);
+    }
+
+
 }
